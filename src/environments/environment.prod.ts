20b090c426f179b6fe4fb2b7685a17bbad219540
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-03-01 17:31:22 -0500

    Fix API URL duplication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // ⚠️ ESTA URL SE REEMPLAZARÁ AUTOMÁTICAMENTE AL HACER BUILD
-  apiUrl: 'https://backendvina-production.up.railway.app/api',  // Railway resolverá esto automáticamente
+  apiUrl: 'https://backendvina-production.up.railway.app',  // Railway resolverá esto automáticamente
   logLevel: 'error',
   apiUrlPHP: 'https://sunatphp-production.up.railway.app'  // ✅ SIN /php 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // frontend_dsi6/src/environments/environment.ts
 export const environment = {
   production: false,
-    apiUrl: 'http://localhost:4000/api',  // ✅ VERIFICAR SI YA TIENE /api  // URL de tu backend Node.js
+    apiUrl: 'http://localhost:4000',  // ✅ VERIFICAR SI YA TIENE /api  // URL de tu backend Node.js
   // Nivel de logs (debug = muchos logs)
     logLevel: 'debug',
     apiUrlPHP: 'http://localhost:8000' // URL de tu backend PHP (si lo usas)
