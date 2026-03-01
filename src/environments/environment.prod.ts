a97d3ed43ade78dffaf01433cf69e37c951ddd61
PaoloFumachi <cesarfumachi2002@gmail.com>
2026-02-28 20:55:01 -0500

    Update production URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // ⚠️ ESTA URL SE REEMPLAZARÁ AUTOMÁTICAMENTE AL HACER BUILD
-  apiUrl: '/api',  // Railway resolverá esto automáticamente
+  apiUrl: 'https://backendvina-production.up.railway.app/api',  // Railway resolverá esto automáticamente
   logLevel: 'error',
-  apiUrlPHP: '/php' // Proxy a PHP service
+  apiUrlPHP: 'https://sunatphp-production.up.railway.app'  // ✅ SIN /php 
 };
